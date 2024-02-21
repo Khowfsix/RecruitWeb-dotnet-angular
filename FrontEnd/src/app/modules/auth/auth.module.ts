349d5d5a96e9744ffc9f3a13e4e8b022e016cc3a
khowfsix <lyhongphat261202@gmail.com>
2024-02-22 01:32:18 +0700

    login + register template

--- FrontEnd/src/app/modules/auth/auth.module.ts
+++ FrontEnd/src/app/modules/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-	declarations: [],
-	imports: [CommonModule],
+	declarations: [
+		// Khai báo các component của authModule ở đây
+	],
+	imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
--- FrontEnd/src/app/modules/auth/pages/register/register.component.ts
+++ FrontEnd/src/app/modules/auth/pages/register/register.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-register',
 	standalone: true,
-	imports: [],
+	imports: [MatCardModule, MatFormFieldModule, MatButtonModule],
 	templateUrl: './register.component.html',
 	styleUrl: './register.component.scss',
 })
