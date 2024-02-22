ea1b9effa2c45d9064748e7d320689145cb9cb9e
khowfsix <lyhongphat261202@gmail.com>
2024-02-22 15:11:30 +0700

    temp

--- FrontEnd/src/app/layout/footer/footer.component.ts
+++ FrontEnd/src/app/layout/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
 	selector: 'app-footer',
 	standalone: true,
-	imports: [],
+	imports: [MatGridListModule, MatIcon],
 	templateUrl: './footer.component.html',
 	styleUrl: './footer.component.css',
 })
