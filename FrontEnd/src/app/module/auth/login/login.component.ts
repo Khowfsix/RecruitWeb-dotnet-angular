80826a1d424e3d96b36a8115d525a738bf843162
khowfsix <lyhongphat261202@gmail.com>
2024-02-24 23:16:56 +0700

    login page template

--- FrontEnd/src/app/module/auth/login/login.component.ts
+++ FrontEnd/src/app/module/auth/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
 	selector: 'app-login',
 	standalone: true,
-	imports: [MatCardModule, MatFormFieldModule],
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.scss',
+	imports: [MatCardModule, MatFormFieldModule, MatIcon, MatGridListModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+	hide: boolean = true;
+}
