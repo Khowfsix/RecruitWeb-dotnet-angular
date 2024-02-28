7a81ee371ecc06a0481047a70fc80a436b4a9474
khowfsix <lyhongphat261202@gmail.com>
2024-02-28 19:12:49 +0700

    create component

---
+++ FrontEnd/src/app/module/auth/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+	selector: 'app-profile',
+	standalone: true,
+	imports: [MatCardModule],
+	templateUrl: './profile.component.html',
+	styleUrl: './profile.component.scss',
+})
+export class ProfileComponent {}
