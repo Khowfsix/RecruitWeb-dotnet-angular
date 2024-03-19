0b8f52d7110a3bdc89a427f19d2bf250c975690e
khowfsix <lyhongphat261202@gmail.com>
2024-03-19 21:27:48 +0700

    con me no

---
+++ FrontEnd/src/app/core/constants/token.constants.ts
@@ -0,0 +1,7 @@
+export enum nameTypeInToken {
+	username = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name',
+	jti = 'jti',
+	roles = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role',
+	exp = 'exp',
+	aud = 'aud',
+}
