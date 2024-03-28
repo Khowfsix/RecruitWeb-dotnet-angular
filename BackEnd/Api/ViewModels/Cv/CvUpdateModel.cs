b2ee5910c052ca0cfa2e3cff1a3866e4e51a8163
khowfsix <lyhongphat261202@gmail.com>
2024-03-28 20:25:46 +0700

    fix cv backend

--- BackEnd/Api/ViewModels/Cv/CvUpdateModel.cs
+++ BackEnd/Api/ViewModels/Cv/CvUpdateModel.cs
@@ -1,27 +1,30 @@
 using Api.ViewModels.Certificate;
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.ViewModels.Cv
 {
     public class CvUpdateModel
     {
+        [Required]
         public Guid Cvid { get; set; }
+        [Required]
         public Guid CandidateId { get; set; }
         public string? Experience { get; set; }
         public string? CvPdf { get; set; }
 
-        public string CvName { get; set; }
+        [Required]
+        public string CvName { get; set; } = string.Empty;
+        public string? Introduction { get; set; } = string.Empty;
+        public string? Education { get; set; } = string.Empty;
 
-        public string Introduction { get; set; }
-
-        public string Education { get; set; }
-
-        public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; } = false;
         public IList<CvSkillRelationUpdateModel> Skills { get; set; } = new List<CvSkillRelationUpdateModel>();
         public IList<CertificateUpdateModel> Certificates { get; set; } = new List<CertificateUpdateModel>();
     }
 
     public class CvSkillRelationUpdateModel
     {
+        [Required]
         public Guid SkillId { get; set; }
         public int ExperienceYear { get; set; }
     }
