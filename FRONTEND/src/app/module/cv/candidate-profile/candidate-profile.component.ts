1081c72a502f2fce9119046ee9eb1f2e10ceb98d
khowfsix <lyhongphat261202@gmail.com>
2024-04-05 01:37:11 +0700

    fix: returnUrl navigate to returnUrl after login

--- FRONTEND/src/app/module/cv/candidate-profile/candidate-profile.component.ts
+++ FRONTEND/src/app/module/cv/candidate-profile/candidate-profile.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'app-candidate-profile',
 	standalone: true,
-	imports: [],
+	imports: [
+		MatDividerModule,
+		MatButtonModule,
+		NgbProgressbarModule
+	],
 	templateUrl: './candidate-profile.component.html',
 	styleUrl: './candidate-profile.component.css'
 })
-export class CandidateProfileComponent {
-
-}
+export class CandidateProfileComponent { }
